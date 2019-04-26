92fb34dd562b99e41a4aea9e536db37ee42b0949
Bruno Molteni <34241947+stamby@users.noreply.github.com>
2019-04-26 12:11:21 -0300

    Agregar descripción

--- empleado.h
+++ empleado.h
@@ -1,6 +1,6 @@
 /*
-empleado: mantiene y muestra los datos de empleados
-en la pantalla, usando funciones de pantalla.h.
+empleado: mantiene y muestra datos de empleados en
+la pantalla, usando funciones de pantalla.h.
 */
 
 #ifndef EMPLEADO_H_INCLUDED
