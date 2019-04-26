c33b404f37622c7a0e3a6692e4cd30040d04e3dd
Bruno Molteni <34241947+stamby@users.noreply.github.com>
2019-04-26 12:18:07 -0300

    Agregar descripción y comentarios

--- empleado.h
+++ empleado.h
@@ -13,10 +13,15 @@ la pantalla, usando funciones de pantalla.h.
 #include "pantalla.h"
 
 namespace empleado {
+    // Archivo que guarda vector de estructura::empleado
     const char archivo[] = "empleados.bin";
+    // Recuento de empleados
     int empleados_c = 0;
+    // Contenido de archivo una vez cargado en memoria
     estructuras::empleado *empleados;
 
+    // Declara estructura::lista a partir de estructura::empleado,
+    // devolviendo un puntero a la estructura::lista
     estructuras::lista *empleado_a_lista(
             estructuras::empleado *empleado) {
         char *horas_char = new char[32];
@@ -34,18 +39,22 @@ namespace empleado {
         return empleado_lista;
     }
 
+    // Borra la lista devuelta por empleado_a_lista
+    // de la memoria
     void liberar_lista(
             estructuras::lista *empleado_lista) {
         delete[] empleado_lista->v[3];
         delete empleado_lista;
     }
 
+    // Borra datos de vector de empleados
     void liberar() {
         empleados_c = 0;
         delete[] empleados;
         empleados = NULL;
     }
 
+    // Puebla vector de estructura::empleado, leyendo archivo
     void cargar() {
         if (empleados_c > 0)
             liberar();
@@ -79,7 +88,8 @@ namespace empleado {
         std::fclose(fp);
     }
 
-    // Devuelve la cantidad de bytes escritos
+    // Guarda vector de estructura::empleado en archivo,
+    // reemplazando sus datos anteriores
     size_t guardar(estructuras::empleado *empleados) {
         FILE *fp;
         fp = std::fopen(archivo, "wb");
@@ -92,6 +102,7 @@ namespace empleado {
         return devolver;
     }
 
+    // Muestra pantalla empleado
     void mostrar() {
         // Mostrar titulo
         pantalla::mostrar_lista_vertical(
@@ -153,7 +164,6 @@ namespace empleado {
             pantalla::mostrar_seleccione(
                     etiquetas::empleado_seleccionar,
                     &ocurrencias);
-            liberar();
         }
     }
 }
