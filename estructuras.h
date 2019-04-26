22f27329799cacec14b620cbee851515ca196345
Bruno Molteni <34241947+stamby@users.noreply.github.com>
2019-04-26 12:21:39 -0300

    Agregar descripción y comentarios

--- estructuras.h
+++ estructuras.h
@@ -1,9 +1,15 @@
+/*
+estructuras: estructuras usadas desde otros archivos.
+*/
+
 #ifndef ESTRUCTURAS_H_INCLUDED
 #define ESTRUCTURAS_H_INCLUDED
 
 #include "enums.h"
 
 namespace estructuras {
+    // c es el recuento de campos en v,
+    // v es un array de arrays char
     struct lista {
         const int c;
         const char *v[12];
