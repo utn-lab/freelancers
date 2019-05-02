958a2ac835457018bc4e386dc4808ffde0c315f4
Bruno <stamblonnocorporation@hotmail.com>
2019-05-02 15:48:15 -0300

    Agregar comentarios

--- main.cpp
+++ main.cpp
@@ -8,10 +8,15 @@ main: punto de partida para ejecucion del programa.
 #include "sector.h"
 
 int main(int argc, char **argv) {
+    // Cargar datos de archivos o por defecto
     empleado::cargar();
     sector::cargar();
+
+    // Ir al menu y permanecer en loop hasta que el usuario elija salir
     enums::pantalla pantalla = enums::MENU;
     while (pantalla != enums::NINGUNO)
         pantalla = interfaz::ir_a(pantalla);
+
+    // Limpiar empleados inactivos del archivo empleados::archivo
     empleado::guardar_solo_activos();
 }
